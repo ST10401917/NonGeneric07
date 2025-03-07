d969c81894730c16dceda727c2dc962a2e9ff33d
ST10401917 <ST10401917@vcconnect.edu.za>
2025-03-07 14:23:35 +0200

    Add project files.

---
+++ NonGeneric07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGeneric07 // push to github
+{
+    class NonGenericDictionaryExample
+    {
+        public static void Main()
+        {
+            Hashtable students = new Hashtable();
+            students.Add(1, "John");
+            students.Add(2, "Emma");
+            students.Add(3, 25); // mixed types allowed
+
+            foreach (DictionaryEntry entry in students)
+            {
+                Console.WriteLine($"ID: {entry.Key}, Name: {entry.Value}");
+            }
+        }
+    }
+}
